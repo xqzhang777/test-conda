bbe371fe431e2ad6daa905d074ecac18ee2fe9f7
xqzhang777 <105519489+xqzhang777@users.noreply.github.com>
2026-01-20 16:08:23 -0500

    Comment out cctbx import error handling
    Comment out cctbx import error handling and related path adjustments.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ from pathlib import Path
 
 "Hello from conda"
 
-if Path("/home/appuser").exists():
-    # essential to avoid cctbx import errors
-    target = Path("/home/appuser/venv/share/cctbx")
-    if not target.exists():
-        target.symlink_to("/home/appuser/.conda/share/cctbx")
+# if Path("/home/appuser").exists():
+#     # essential to avoid cctbx import errors
+#     target = Path("/home/appuser/venv/share/cctbx")
+#     if not target.exists():
+#         target.symlink_to("/home/appuser/.conda/share/cctbx")
 
-    sys.path += ["/home/appuser/venv/lib/python3.9/lib-dynload"]
-    os.environ["PATH"] += os.pathsep + "/home/appuser/.conda/bin"
+#     sys.path += ["/home/appuser/venv/lib/python3.9/lib-dynload"]
+#     os.environ["PATH"] += os.pathsep + "/home/appuser/.conda/bin"
 
 
 import streamlit as st
